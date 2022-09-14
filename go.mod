a49b75de5a6d57d183288fe2dde586f217d532f5
Mattia de Filippo <mattia.de.filippo@datamir.info>
2022-09-14 15:48:35 +0200

    simple pub sub

--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/defilippomattia/football-iot
 
 go 1.18
+
+require (
+	github.com/hamba/avro/v2 v2.1.0
+	github.com/nats-io/nats.go v1.16.1-0.20220906180156-a1017eec10b0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.0 // indirect
+	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ publisher.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/hamba/avro/v2"
+	"github.com/nats-io/nats.go"
+)
+
+func readPlayerCsv(csvPath string) {
+
+	nc, _ := nats.Connect(nats.DefaultURL)
+
+	type SensorReading struct {
+		TIMESTAMP     string  `avro:"timestamp"`
+		TAGID         int32   `avro:"tag_id"`
+		XPOS          float64 `avro:"x_pos"`
+		YPOS          float64 `avro:"y_pos"`
+		HEADING       float64 `avro:"heading"`
+		DIRECTION     float64 `avro:"direction"`
+		ENERGY        float64 `avro:"energy"`
+		SPEED         float64 `avro:"speed"`
+		TOTALDISTANCE float64 `avro:"total_distance"`
+	}
+
+	schema, err := avro.Parse(`{
+		"type": "record",
+		"name": "reading",
+		"namespace": "org.hamba.avro",
+		"fields" : [
+			{"name": "timestamp", "type": "string"},
+			{"name": "tag_id", "type": "int"},
+			{"name": "x_pos", "type": "double"},
+			{"name": "y_pos", "type": "double"},
+			{"name": "heading", "type": "double"},
+			{"name": "direction", "type": "double"},
+			{"name": "energy", "type": "double"},
+			{"name": "speed", "type": "double"},
+			{"name": "total_distance", "type": "double"}
+		]
+	}`)
+	if err != nil {
+		fmt.Println(err)
+	}
+	csvFile, err := os.Open(csvPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, line := range csvLines {
+		timestampL := line[0]
+		tagIdL, _ := strconv.Atoi(line[1])
+		xPosL, _ := strconv.ParseFloat(line[2], 64)
+		yPosL, _ := strconv.ParseFloat(line[3], 64)
+		headingL, _ := strconv.ParseFloat(line[4], 64)
+		directionL, _ := strconv.ParseFloat(line[5], 64)
+		energryL, _ := strconv.ParseFloat(line[6], 64)
+		speedL, _ := strconv.ParseFloat(line[7], 64)
+		totalDistanceL, _ := strconv.ParseFloat(line[8], 64)
+
+		in := SensorReading{
+			TIMESTAMP:     timestampL,
+			TAGID:         int32(tagIdL),
+			XPOS:          xPosL,
+			YPOS:          yPosL,
+			HEADING:       headingL,
+			DIRECTION:     directionL,
+			ENERGY:        energryL,
+			SPEED:         speedL,
+			TOTALDISTANCE: totalDistanceL,
+		}
+
+		data, err := avro.Marshal(schema, in)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(data)
+		nc.Publish("football-iot.players", data)
+		time.Sleep(3 * time.Second)
+
+		// out := SensorReading{}
+		// err = avro.Unmarshal(schema, data, &out)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// fmt.Println(out)
+	}
+
+}
+
+func main() {
+
+	readPlayerCsv("./resources/sensor-data/player_x.csv")
+
+}
